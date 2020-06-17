92504b99b74c39bbb20afd305ef2c02e181d96a9
broughtj <broughtj@gmail.com>
2020-06-16 21:27:12 -0600

    Fixing indentation.

--- lua/plokoon/init.lua
+++ lua/plokoon/init.lua
@@ -1,6 +1,6 @@
 local function greet()
-  quotes = {"Your emotions are clouding your judgement.",
-	        "We are not alone here.",
+  quotes = {"Your emotions are clouding your judgement.", 
+            "We are not alone here.",
 			"Don't shoot or they'll panic.",
 			"When you ask for trouble, you should not be surprised when it finds you.",
 			"Great job, Master Skywalker. Your leadership skills are most impressive."
