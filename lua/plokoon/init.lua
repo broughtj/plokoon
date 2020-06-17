3566022007bab427b800c8ba1f3771f86ddcc602
broughtj <broughtj@gmail.com>
2020-06-16 21:23:46 -0600

    Adding a random quote from Plo Koon.

--- lua/plokoon/init.lua
+++ lua/plokoon/init.lua
@@ -1,5 +1,12 @@
 local function greet()
-	print("Plo Koon says: Your emotions are clouding your judgement.")
+  quotes = {"Your emotions are clouding your judgement.",
+	        "We are not alone here.",
+			"Don't shoot or they'll panic.",
+			"When you ask for trouble, you should not be surprised when it finds you.",
+			"Great job, Master Skywalker. Your leadership skills are most impressive."
+		  }
+  idx = math.random(5)
+  print("Plo Koon says: " .. quotes[idx])
 end
 
 return {greet = greet}
