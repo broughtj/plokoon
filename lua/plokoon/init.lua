743165571cc7f97d9ef36cb3fed2bec23d4b7bbc
broughtj <broughtj@gmail.com>
2020-06-16 21:38:49 -0600

    Adding a new quote.

--- lua/plokoon/init.lua
+++ lua/plokoon/init.lua
@@ -1,11 +1,12 @@
 local function greet()
   quotes = {"Your emotions are clouding your judgement.", 
             "We are not alone here.",
-			"Don't shoot or they'll panic.",
-			"When you ask for trouble, you should not be surprised when it finds you.",
-			"Great job, Master Skywalker. Your leadership skills are most impressive."
-		  }
-  idx = math.random(5)
+            "Don't shoot or they'll panic.",
+            "When you ask for trouble, you should not be surprised when it finds you.",
+            "Great job, Master Skywalker. Your leadership skills are most impressive.",
+            "I don't believe in chance, Commander."
+  }
+  idx = math.random(6)
   print("Plo Koon says: " .. quotes[idx])
 end
 
